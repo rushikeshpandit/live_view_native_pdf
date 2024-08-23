8db78b407ed64b09b188e347c5b92bdfb09d68b2
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-08-23 21:47:28 +0530

    - Deleted messed-up code

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,13 +21,12 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
-import PDF from './hooks/pdf';
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
    hooks: {
-      PDF
+      
     },
   params: {_csrf_token: csrfToken}
 })
