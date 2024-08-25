c48942072aec4777a7c5984275f8dc17de4a52c0
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-08-25 13:40:58 +0530

    - added upload_file directory to static folder

--- lib/live_view_native_pdf_web.ex
+++ lib/live_view_native_pdf_web.ex
@@ -17,7 +17,7 @@ defmodule LiveViewNativePdfWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt upload_file)
 
   def router do
     quote do
